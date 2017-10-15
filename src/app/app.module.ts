8220b8a6efd94b49887b91452f4dcb048fd7c501
William KLEIN <wklein@infinitesquare.com>
2017-10-15 22:21:25 +0200

    remove preboot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { BrowserPrebootModule } from 'preboot/browser';
 
 @NgModule({
   declarations: [
@@ -10,8 +9,7 @@ import { BrowserPrebootModule } from 'preboot/browser';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'app-id' }),
-    FormsModule,
-    BrowserPrebootModule.replayEvents()
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -2,16 +2,11 @@ import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
-import { ServerPrebootModule } from 'preboot/server';
 
 @NgModule({
   imports: [
     ServerModule,
-    AppModule,
-    ServerPrebootModule.recordEvents({ appRoot: 'app-root' , eventSelectors : [
-      { selector: 'input,textarea', events: ['keypress', 'keyup', 'keydown', 'input', 'change'] },
-      { selector: 'li', events: ['click'] },
-    ]})
+    AppModule
   ],
   bootstrap: [
     AppComponent
